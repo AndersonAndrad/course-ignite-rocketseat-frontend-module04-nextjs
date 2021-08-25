e23af7a5ec430c62b1f40d22db80b5d0a2f19012
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-25 18:43:35 -0300

    config  - adding react-hooks-form in project

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,16 +1,20 @@
 // chakra-ui
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react'
+// dependencies
+import { forwardRef, ForwardRefRenderFunction } from 'react'
 // types
 interface InputProps extends ChakraInputProps {
   name: string
   label?: string
 }
 
-export default function Input ( { name, label, ...rest }: InputProps ) {
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ( { name, label, ...rest }, ref ) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput name={name} id={name} focusBorderColor='purple.500' bgColor='gray.900' variant='field' _hover={{ bgColor: 'gray.900' }} size='lg' {...rest} />
+      <ChakraInput name={name} id={name} focusBorderColor='purple.500' bgColor='gray.900' variant='field' _hover={{ bgColor: 'gray.900' }} size='lg' {...rest} ref={ref} />
     </FormControl>
   )
 }
+
+export const Input = forwardRef( InputBase )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 // chakra-ui
 import { Button, Flex, Stack } from '@chakra-ui/react'
+// dependencies
+import { useForm } from 'react-hook-form'
 // shared components
-import Input from '../components/Form/Input'
+import { Input } from '../components/Form/Input'
+// types
 
 export default function SingIn () {
+  const { register, handleSubmit, formState } = useForm()
+  const { errors } = formState
+
+  function handleSingIn ( values ) { }
+
   return (
     <Flex w='100vw' h='100vh' align='center' justify='center'>
-      <Flex as='form' w='100%' maxW={360} bg='gray.800' p='8' borderRadius={8} flexDir='column' >
+      <Flex as='form' w='100%' maxW={360} bg='gray.800' p='8' borderRadius={8} flexDir='column' onSubmit={handleSubmit( handleSingIn )} >
         <Stack spacing='4'>
-          <Input name='email' label='Email' type='email' />
-          <Input name='password' label='Password' type='password' />
+          <Input name='email' label='Email' type='email' {...register( 'email' )} />
+          <Input name='password' label='Password' type='password' {...register( 'password' )} />
         </Stack>
         <Button type='submit' mt='6' colorScheme='purple'>Sing in</Button>
       </Flex>
