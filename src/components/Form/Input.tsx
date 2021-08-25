ca1c0de88aef53f7a9da152c363cbd6a0311c12e
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-25 18:56:43 -0300

    feature - form - add validations in singin page

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,18 +1,22 @@
 // chakra-ui
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react'
 // dependencies
 import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { FieldError } from 'react-hook-form'
 // types
 interface InputProps extends ChakraInputProps {
   name: string
   label?: string
+  error?: FieldError
 }
 
-export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ( { name, label, ...rest }, ref ) => {
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ( { name, label, error = null, ...rest }, ref ) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput name={name} id={name} focusBorderColor='purple.500' bgColor='gray.900' variant='field' _hover={{ bgColor: 'gray.900' }} size='lg' {...rest} ref={ref} />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,38 @@
-// chakra-ui
 import { Button, Flex, Stack } from '@chakra-ui/react'
-// dependencies
-import { useForm } from 'react-hook-form'
-// shared components
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 import { Input } from '../components/Form/Input'
 // types
+type SingInFormData = {
+  email: string
+  password: string
+}
+
+const singInFormSchema = yup.object().shape( {
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+} )
 
 export default function SingIn () {
-  const { register, handleSubmit, formState } = useForm()
-  const { errors } = formState
+  const { register, handleSubmit, formState } = useForm( {
+    resolver: yupResolver( singInFormSchema ),
+  } )
 
-  function handleSingIn ( values ) { }
+  const { errors, isSubmitting } = formState
+
+  const handleSingIn: SubmitHandler<SingInFormData> = ( values ) => { }
 
   return (
     <Flex w='100vw' h='100vh' align='center' justify='center'>
-      <Flex as='form' w='100%' maxW={360} bg='gray.800' p='8' borderRadius={8} flexDir='column' onSubmit={handleSubmit( handleSingIn )} >
+      <Flex as='form' w='100%' maxW={360} bg='gray.800' p='8' borderRadius={8} flexDir='column' onSubmit={handleSubmit( handleSingIn )}>
         <Stack spacing='4'>
-          <Input name='email' label='Email' type='email' {...register( 'email' )} />
-          <Input name='password' label='Password' type='password' {...register( 'password' )} />
+          <Input name='email' label='Email' type='email' {...register( 'email' )} error={errors.email} />
+          <Input name='password' label='Password' type='password' {...register( 'password' )} error={errors.password} />
         </Stack>
-        <Button type='submit' mt='6' colorScheme='purple'>Sing in</Button>
+        <Button type='submit' mt='6' colorScheme='purple' isLoading={isSubmitting}>
+          Sing in
+        </Button>
       </Flex>
     </Flex>
   )
