488f013df74ffa1cfe038ead316783fe5b94560a
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-24 22:04:12 -0300

    style | responsive - add responsive in create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,15 +9,15 @@ export default function CreateUser () {
       <Header />
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['6', '8']}>
           <Heading size='lg' fontWeight='normal'>Create user</Heading>
           <Divider my='6' borderColor='gray.700' />
           <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%' >
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%' >
               <Input name='name' label='Full name' />
               <Input name='email' label='Email' type='email' />
             </SimpleGrid>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%' >
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%' >
               <Input name='password' label='Password' type='password' />
               <Input name='password_confirmation' label='Confirm password' type='password' />
             </SimpleGrid>
