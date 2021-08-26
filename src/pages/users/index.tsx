199d0706dafac9a81c97b21540aad3316b8eb796
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-26 19:23:38 -0300

    feature - User - add call to users in react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,21 +1,20 @@
 // chakra-ui
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react"
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react"
 // dependencies
 import Link from 'next/link'
-import { useEffect } from 'react'
 // icons
 import { RiAddLine } from "react-icons/ri"
+import { useQuery } from 'react-query'
 // shared components
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList () {
-  useEffect( () => {
-    fetch( 'http://localhost:3000/api/users' )
-      .then( res => res.json() )
-      .then( data => console.log( data ) )
-  }, [] )
+  const { data, isLoading, error } = useQuery( 'users', async () => {
+    const response = await fetch( 'http://localhost:3000/api/users' )
+    return await response.json()
+  } )
 
   const isWideVersion = useBreakpointValue( {
     base: false,
@@ -37,32 +36,44 @@ export default function UserList () {
             </Link>
 
           </Flex>
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th pc={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='purple' />
-                </Th>
-                <Th>Name</Th>
-                {isWideVersion && <Th>CreatedAt</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='purple' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Anderson Andrade</Text>
-                    <Text fontSize='sm' color='gray.300'>anderson_andrade_@outlook.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 April, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Error to load data</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th pc={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='purple' />
+                    </Th>
+                    <Th>Name</Th>
+                    {isWideVersion && <Th>CreatedAt</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='purple' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Anderson Andrade</Text>
+                        <Text fontSize='sm' color='gray.300'>anderson_andrade_@outlook.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 April, 2021</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
