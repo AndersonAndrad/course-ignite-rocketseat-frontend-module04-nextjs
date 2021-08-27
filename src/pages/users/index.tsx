400bec271f563c41e5f91773808d0c0695fe9fac
AndersonAndrad <anderson_andrade_@outlook.com>
2021-08-27 16:46:40 -0300

    config - add axios dependence in project, and configure axios to call all users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,11 +9,12 @@ import { useQuery } from 'react-query'
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
+// services
+import { api } from '../../services/api.service'
 
 export default function UserList () {
   const { data, isLoading, isFetched, error } = useQuery( 'users', async () => {
-    const response = await fetch( 'http://localhost:3000/api/users' )
-    const data = await response.json()
+    const { data } = await api.get( '/users' )
 
     const users = data.users.map( user => {
       return {
