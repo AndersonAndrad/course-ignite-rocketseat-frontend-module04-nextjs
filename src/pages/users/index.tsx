fe0b7ba467120a4ceed861755f923ae84740ac4c
AndersonAndrad <anderson_andrade_@outlook.com>
2021-08-27 16:42:04 -0300

    style - show load data during fetch data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,7 @@ import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList () {
-  const { data, isLoading, error } = useQuery( 'users', async () => {
+  const { data, isLoading, isFetched, error } = useQuery( 'users', async () => {
     const response = await fetch( 'http://localhost:3000/api/users' )
     const data = await response.json()
 
@@ -43,7 +43,10 @@ export default function UserList () {
 
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Users</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Users
+              {!isLoading && isFetched && <Spinner size='sm' color='gray.500' ml='4' />}
+            </Heading>
             <Link href='/users/create' passHref>
               <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiAddLine} />}>Create new</Button>
             </Link>
